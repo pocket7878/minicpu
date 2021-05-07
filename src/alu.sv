e050f01c6541e332e769e3cf426e7ce1f1e758df
pocket <poketo7878@gmail.com>
2021-05-07 10:49:11 +0900

    Support nop by sll

--- src/alu.sv
+++ src/alu.sv
@@ -11,6 +11,7 @@ always_comb begin
     3'b000: y = a & b;
     3'b001: y = a | b;
     3'b010: y = a + b;
+    3'b011: y = a << b;
     3'b100: y = a & ~b;
     3'b101: y = a | ~b;
     3'b110: y = a - b;
